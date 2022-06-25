2538bdf705e780a196150fc3d4888c3681a36e25
zawakasutera <zawazawanomorichan@outlook.jp>
2022-06-26 03:09:26 +0900

    前準備テンプレート

---
+++ pre.hpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+// #include <atcoder/modint>
+
+using namespace std;
+// using namespace atcoder;
+using ll = long long;
+using Int = long long;
+// using mint = modint998244353;
+// using mint = modint1000000007;
+
+#define all(x) begin(x), end(x)
