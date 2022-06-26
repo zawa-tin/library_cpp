db09113ea5d0c506313871a0a8b0f4eb1552b7c8
zawakasutera <zawazawanomorichan@outlook.jp>
2022-06-27 03:09:40 +0900

    高速化

--- pre.hpp
+++ pre.hpp
@@ -1,3 +1,6 @@
+#pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+#pragma GCC optimize("unroll-loops")
 #include <bits/stdc++.h>
 // #include <atcoder/modint>
 
@@ -7,5 +10,14 @@ using ll = long long;
 using Int = long long;
 // using mint = modint998244353;
 // using mint = modint1000000007;
+// using pll = pair<ll, ll>
 
 #define all(x) begin(x), end(x)
+
+// const ll supl = 1e18 / 2 - 10000000000;
+// const ll supi = 1e9 / 2 - 100000;
+
+int main() {
+    ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+}
