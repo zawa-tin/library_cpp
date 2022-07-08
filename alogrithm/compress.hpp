f483c83170a26077fb6afb6596ba039ff6e3b57d
zawakasutera <zawazawanomorichan@outlook.jp>
2022-07-08 13:24:22 +0900

    zaatu

--- alogrithm/compress.hpp
+++ alogrithm/compress.hpp
@@ -1,26 +1,12 @@
 #include <vector>
 #include <algorithm>
 
-template<typename T>
-class compress {
-
-private:
-    vector<T> cp;
-
-public:
-    compress(vector<T>& a) {
-        cp = a;
-        sort(cp.begin(), cp.end());
-        cp.erase(unique(cp.begin(), cp.end()), cp.end());
-    }
-
-    int get(int x) {
-        return lower_bound(cp.begin(), cp.end(), x) - cp.begin();
-    }
-
-    vector<int> get(vector<T>& a) {
-        vector<int> res(a.size());
-        for (int i = 0 ; i < a.size() ; i++) res[i] = lower_bound(cp.begin(), cp.end(), a[i]) - cp.begin();
-        return res;
-    }
-};
+template <typename T>
+vector<int> compress(const vector<T>& a) {
+    vector<T> cp = a;
+    sort(cp.begin(), cp.end());
+    cp.erase(unique(cp.begin(), cp.end()), cp.end());
+    vector<int> res(a.size());
+    for (int i = 0 ; i < a.size() ; i++) res[i] = lower_bound(cp.begin(), cp.end(), a[i]) - cp.begin();
+    return res;
+}
