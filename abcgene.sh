0c63ae47bb468d5a27298bed3e1fa44e6d7f800a
zawakasutera <90969078+zawa-tin@users.noreply.github.com>
2022-07-15 21:47:35 +0900

    add abcgene

---
+++ abcgene.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+read id
+
+head=`expr $id / 100`
+echo $head
+
+if [ $head -eq 0 ]; then
+    cd ../atcoder/ABC0
+elif [ $head -eq 1 ]; then
+    cd ../atcoder/ABC1
+else
+    cd ../atcoder/ABC2
+fi
+
+mkdir ABC${id}
+cd ABC${id}
+
+dirnames=`echo 'ABCDEFGH' | fold -s1`
+for dirname in $dirnames
+do
+    mkdir $dirname
+    cd $dirname
+    cp ../../../../library_cpp/pre.hpp main.cpp
+    cp ../../../../library_cpp/Makefile .
+    cd ../
+done
